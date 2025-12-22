143c76b9e0150ac72042a5713954484c9352c3b2
MAJinTao112 <398886755@qq.com>
2025-12-22 22:35:34 +0800

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Login from '../views/Login.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/login',
-      name: 'Login',
-      component: Login,
-    },
+
   ],
 })
 
